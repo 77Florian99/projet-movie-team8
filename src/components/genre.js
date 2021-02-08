4234f1b4fd2c0b9f36770597db0f1d461599bc61
77florian99 <flo.sarrazin77@gmail.com>
2021-02-08 11:14:55 +0100

    Update genre.js

--- src/components/genre.js
+++ src/components/genre.js
@@ -1,2 +1,50 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
+
+export const genreFilm = (props) => {
+    const {genre} = props;
+    return(
+        <View style={styles.main_container}>
+            <View style={styles.main_information_container}>
+                <View style={styles.content_container}>
+                    <View>
+                        <Text style={styles.title_text}>{film.title}</Text>
+                    </View>
+                </View>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    main_container: {
+        height: 150,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    content_container: {
+        flex: 1,
+        margin: 5,
+        justifyContent: 'center',
+    },
+    main_information_container: {
+        flexDirection: 'row'
+    },
+    image: {
+        width: 80,
+        height: 120,
+        margin: 5,
+    },
+    title_text: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        flexWrap: 'wrap',
+        paddingRight: 5
+    },
+    picto: {
+        width: 30,
+        height: 30,
+    }
+})

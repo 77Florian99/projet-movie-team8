d523b0ee74a7b95340d7e198f12efe5117346438
julesburcez <jules.burcez83@gmail.com>
2021-02-11 23:38:47 +0100

    Mise à jour de la BottomNav avec la SearchScreen

--- src/navigation/MainBottomNavigator.js
+++ src/navigation/MainBottomNavigator.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-// import {SearchScreen} from "../screen/SearchScreen";
+import SearchScreen from "../screen/SearchScreen";
 import { HomePage } from '../screen/HomePage';
-import { DetailScreen } from '../screen/DetailScreen';
 
 const TabNavigator = createBottomTabNavigator();
 
@@ -10,8 +9,7 @@ export const MainBottomNavigator = () => {
     return (
         <TabNavigator.Navigator>
             <TabNavigator.Screen name="Home" component={HomePage} />
-            <TabNavigator.Screen name="Detail" component={DetailScreen} />
-            {/* <TabNavigator.Screen name="Search" component={SearchScreen} /> */}
+            <TabNavigator.Screen name="Search" component={SearchScreen} />
         </TabNavigator.Navigator>
     )
 }
